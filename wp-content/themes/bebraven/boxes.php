900ac987f6140d7512e464179b15b98245242c48
ehudtal <ehud.tal@hefetzandtal.com>
2017-11-25 14:29:50 -0500

    boxes are dynamic

--- wp-content/themes/bebraven/boxes.php
+++ wp-content/themes/bebraven/boxes.php
@@ -3,33 +3,33 @@
  * Template for boxes that display things like donor logos
  * This is called from index.php when the component format is 'boxes'
  */
-?>
 
-<div class="mosaic">
-	<div class="mosaic-element">
-		<div class="box">
-			<div class="box-content">BLAH</div>
-		</div>
-	</div>
-	<div class="mosaic-element">
-		<div class="box">
-			<div class="box-content">BLAH</div>
-		</div>
-	</div>
-	<div class="mosaic-element">
-		<div class="box">
-			<div class="box-content">BLAH</div>
-		</div>
-	</div>
-	<div class="mosaic-element">
-		<div class="box">
-			<div class="box-content">BLAH</div>
-		</div>
-	</div>
-	<div class="mosaic-element">
-		<div class="box">
-			<div class="box-content">BLAH</div>
-		</div>
-	</div>
-</div>
 
+$args = array(
+	'post_type' => 'page',
+	'post_parent' => $post->ID, 
+	'post_status' => 'publish',
+	'orderby' => 'menu_order',
+	'order' => 'ASC',
+);
+
+$boxes_query = new WP_Query( $args );
+
+if ( $boxes_query->have_posts() ) {
+	?>
+	<div class="mosaic">
+		<?php
+			while ( $boxes_query->have_posts() ) {
+				$boxes_query->the_post();
+				?>
+					<div class="mosaic-element">
+						<div class="box">
+							<div class="box-content"><?php the_title();?></div>
+						</div>
+					</div>
+				<?php
+			} // while
+		?>
+	</div>
+	<?php
+} //if
