456757ba85d95c906808e4ddc976b3b42775505b
ehud <ehud.tal@hefetzandtal.com>
2018-09-17 13:51:16 -0400

    New layout for plain pages

--- wp-content/themes/bebraven/index.php
+++ wp-content/themes/bebraven/index.php
@@ -8,7 +8,7 @@ get_header();
 
 
 ?>
-
+<!-- index -->
 <div class="wrap">
 
 	<?php bz_custom_breadcrumbs(); ?>
@@ -97,91 +97,102 @@ get_header();
 
 			} else if ( !empty($post) ) {
 
-				// If it's not the blog page, but it has some data:
+				if ( $post->post_type == 'page' && $post->post_parent == 0) {
+					// If it's some random page like a job posting
+					?>
+					<!-- orphan page -->
+					<section class="component default orphan-page">
+						<?php echo apply_filters('the_content', $post->post_content); ?>
+					</section>
+					<?php
+				} else {
+					echo '<!--tabs for kids -->';
+					// If it's not the blog page or some random page like a job posting, but it still has some data:
 
-				/* In order to accomodate tabbed sub-navigation (e.g. on region pages)
-				 * we collect all the content into the following vars and then print it.
-				 */
+					/* In order to accomodate tabbed sub-navigation (e.g. on region pages)
+					 * we collect all the content into the following vars and then print it.
+					 */
 
-				// Start building the parts of the page:
-				$tabs_menu = '<nav id="tabs-menu" class="tabs-menu"><a href="#parent-tab" class="active">' . $post->post_title . '</a>';
-				$parent_content = '';
-				$tabs_content = '';
+					// Start building the parts of the page:
+					$tabs_menu = '<nav id="tabs-menu" class="tabs-menu"><a href="#parent-tab" class="active">' . $post->post_title . '</a>';
+					$parent_content = '';
+					$tabs_content = '';
 
-				global $container_ID; // $container_ID is defined in the header.php template 
-				
-				$args = array(
-					'post_type' => 'page',
-					'post_parent' => $container_ID, 
-					'post_status' => 'publish',
-					'orderby' => 'menu_order',
-					'nopaging' => 'true',
-					'order' => 'ASC',
-				);
-				
-				$components_query = new WP_Query( $args );
-
-				// Loop through the returned components (sub-pages of this page):
-				if ( $components_query->have_posts() ) {
+					global $container_ID; // $container_ID is defined in the header.php template 
 					
-					while ( $components_query->have_posts() ) {
-						$components_query->the_post();
-						$component_format = (wp_get_post_terms($post->ID, 'format')) ? wp_get_post_terms($post->ID, 'format')[0]->slug : '';
+					$args = array(
+						'post_type' => 'page',
+						'post_parent' => $container_ID, 
+						'post_status' => 'publish',
+						'orderby' => 'menu_order',
+						'nopaging' => 'true',
+						'order' => 'ASC',
+					);
+					
+					$components_query = new WP_Query( $args );
 
-						// NOTE: If you're looking for how the bios are generated, search for include-bios in functions.php
-
-
-						// If this sub-page is supposed to show up as a tab with its own components:
-						if ('tab' == $component_format) {
-
-							// start a buffer for this tab's content
-							ob_start();
-
-							$tabs_menu .= '<a href="#' . $post->post_name . '">' . $post->post_title . '</a>';
-
-
-							echo '<div id="' . $post->post_name . '" class="tab sub-page-tab ' . $post->post_name . '">';
-
-							$args = array(
-								'post_type' => 'page',
-								'post_parent' => $post->ID, 
-								'post_status' => 'publish',
-								'orderby' => 'menu_order',
-								'nopaging' => 'true',
-								'order' => 'ASC',
-							);
-				
-							$tab_components_query = new WP_Query( $args );
-
-							while ( $tab_components_query->have_posts() ) {
-								$tab_components_query->the_post();
-
-								$component_format = (wp_get_post_terms($post->ID, 'format')) ? wp_get_post_terms($post->ID, 'format')[0]->slug : '';
-
-								// Run the data through the content template  
-								// and add that to the contant var 
-								// (via buffer so it doesn't print prematurely):
-
-								include('content.php');
-							}
-
-							echo '</div>';
-
-
-							$tabs_content .= ob_get_clean();
-
-
-							/* Restore original Post Data */
-							wp_reset_postdata();
+					// Loop through the returned components (sub-pages of this page):
+					if ( $components_query->have_posts() ) {
 						
-						} else {
-							// If this content isn't part of a tab, it must be for the parent page, so let's save it there (via a buffer again):
+						while ( $components_query->have_posts() ) {
+							$components_query->the_post();
+							$component_format = (wp_get_post_terms($post->ID, 'format')) ? wp_get_post_terms($post->ID, 'format')[0]->slug : '';
 
-							ob_start();
-							include('content.php');
-							$parent_content .= ob_get_clean(); 
+							// NOTE: If you're looking for how the bios are generated, search for include-bios in functions.php
 
-						}
+
+							// If this sub-page is supposed to show up as a tab with its own components:
+							if ('tab' == $component_format) {
+
+								// start a buffer for this tab's content
+								ob_start();
+
+								$tabs_menu .= '<a href="#' . $post->post_name . '">' . $post->post_title . '</a>';
+
+
+								echo '<div id="' . $post->post_name . '" class="tab sub-page-tab ' . $post->post_name . '">';
+
+								$args = array(
+									'post_type' => 'page',
+									'post_parent' => $post->ID, 
+									'post_status' => 'publish',
+									'orderby' => 'menu_order',
+									'nopaging' => 'true',
+									'order' => 'ASC',
+								);
+					
+								$tab_components_query = new WP_Query( $args );
+
+								while ( $tab_components_query->have_posts() ) {
+									$tab_components_query->the_post();
+
+									$component_format = (wp_get_post_terms($post->ID, 'format')) ? wp_get_post_terms($post->ID, 'format')[0]->slug : '';
+
+									// Run the data through the content template  
+									// and add that to the contant var 
+									// (via buffer so it doesn't print prematurely):
+
+									include('content.php');
+								}
+
+								echo '</div>';
+
+
+								$tabs_content .= ob_get_clean();
+
+
+								/* Restore original Post Data */
+								wp_reset_postdata();
+							
+							} else {
+								// If this content isn't part of a tab, it must be for the parent page, so let's save it there (via a buffer again):
+
+								ob_start();
+								include('content.php');
+								$parent_content .= ob_get_clean(); 
+
+							}
+						} 
 					} // end while
 
 					if ($tabs_content) { 
@@ -195,6 +206,7 @@ get_header();
 					
 					} else {
 						// If there are no tabs, just print the parent's components directly:
+						echo '<!-- parent content only -->';
 						echo $parent_content;
 					
 					}
